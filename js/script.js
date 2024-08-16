f7d6b5494cf46bff97a3f0e3058e1edd5ff021bb
gideongurning31 <gideon.gurning@gmail.com>
2024-08-16 15:50:41 +0700

    Click button functionality

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
-function toNextButton() {
-  alert('toNextButton');
-}
+$(document).ready(function () {
 
-function redirectToSomewhere() {
-  window.location.href = 'https://ibox.co.id/product/IP-11-NP-IB-CON';
-}
+  $('#question-0 > button').click(() => {
+    $("#question-0").css("display", "none");
+    $("#question-1").css("display", "flex");
+  });
 
-$(document).ready(function () {});
+  $('#question-1 > button').click(() => {
+    $("#question-1").css("display", "none");
+    $("#question-2").css("display", "flex");
+  });
+
+  $('#question-2 > button').click(() => {
+    window.location.href = 'https://ibox.co.id/product/IP-11-NP-IB-CON';
+  });
+});
