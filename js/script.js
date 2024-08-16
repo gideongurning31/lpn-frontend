c84ecf8e9acc60510f807a308077dab4c2620a68
gideongurning31 <gideon.gurning@gmail.com>
2024-08-16 15:33:23 +0700

    Buttons slicing desktop

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function toNextButton() {
+  alert('toNextButton');
+}
+
+function redirectToSomewhere() {
+  window.location.href = 'https://ibox.co.id/product/IP-11-NP-IB-CON';
+}
+
+$(document).ready(function () {});
